c5ba6e615ccc4ad573e3fd5760c4b7521947cc98
luvitor <luan.silva524@etec.sp.gov.br>
2023-09-21 21:36:53 -0300

    aga

---
+++ app/src/main/java/br/com/etecia/myapp/exemplo1_2_layout.java
@@ -0,0 +1,14 @@
+package br.com.etecia.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class exemplo1_2_layout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.exemplo1_2_layout);
+    }
+}
